6c77362d94bb7e51714cd3a36680430d8ec3c2ca
MougLee <mou6li@gmail.com>
2021-07-13 09:17:17 +0200

    fix instructions

--- run-project.sh
+++ run-project.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Packaging project ..."
-docker-compose run --rm analyzer sbt assembly
-
 echo "moving jar to workers"
 rm -f spark-cluster/apps/imdb.jar
 cp target/scala-2.12/imdb-assembly-0.1.jar spark-cluster/apps/imdb.jar
